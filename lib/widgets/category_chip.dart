780890f25b4edfda4fab383a2eb4792cfb436ca1
Manjiro <85405446+grocerystoreproject@users.noreply.github.com>
2025-10-13 08:36:13 +0530

    Create category_chip.dart

---
+++ lib/widgets/category_chip.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class CategoryChip extends StatelessWidget {
+  final String text;
+  final Color color;
+  final VoidCallback? onTap;
+
+  const CategoryChip({
+    super.key,
+    required this.text,
+    required this.color,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+        decoration: BoxDecoration(
+          color: color.withOpacity(0.15),
+          borderRadius: BorderRadius.circular(14),
+          border: Border.all(color: color.withOpacity(0.3)),
+        ),
+        child: Text(
+          text,
+          style: TextStyle(
+            color: color,
+            fontSize: 12,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
